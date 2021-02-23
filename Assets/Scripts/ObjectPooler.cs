ba84b6530e82e4f2affb16e48a8b3e88c19cdee8
Juan Travol <38193072+Juan1932@users.noreply.github.com>
2021-02-23 02:53:19 -0300

    Update Object Pooler

--- Assets/Scripts/ObjectPooler.cs
+++ Assets/Scripts/ObjectPooler.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class ObjectPooler : MonoBehaviour
 {
     private static ObjectPooler instance;
-
     private GameObject projectileParentFolder;
+    public List<GameObject> pooledObjects;
+    public List<ObjectPoolItem> itemsToPool;
+
+    [System.Serializable]
+    public class ObjectPoolItem
+    {
+        public GameObject objectToPool;
+        public int amountToPool;
+        public bool shouldExpand = true;
+    }
+    
 
     public static ObjectPooler Instance
     {
@@ -23,7 +33,7 @@ public class ObjectPooler : MonoBehaviour
 
     private GameObject currentItem;
 
-    public void Awake()
+    void Awake()
     {
         if(instance == null)
         {
@@ -34,6 +44,22 @@ public class ObjectPooler : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Start()
+    {
+        pooledObjects = new List<GameObject>();
+
+        foreach(ObjectPoolItem item in itemsToPool)
+        {
+            for (int i = 0; i < item.amountToPool; i++)
+            {
+                GameObject obj = Instantiate(item.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
+        }
+        
     }
 
     public void CreatePool(WeaponType weapon, List<GameObject> currentPool, GameObject projectileParentFolder)
@@ -48,6 +74,29 @@ public class ObjectPooler : MonoBehaviour
         }
 
         projectileParentFolder.name = weapon.name;
+    }
+
+    public GameObject GetPooledObject(string tag)
+    {
+        for (int i=0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy && pooledObjects[i].tag == tag)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        foreach(ObjectPoolItem item in itemsToPool)
+        {
+            if (item.objectToPool.tag == tag && item.shouldExpand)
+            {
+                GameObject obj = Instantiate(item.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+                return obj;
+            }
+        }
+        return null;
     }
 
     public GameObject GetObject(List<GameObject> currentPool)
